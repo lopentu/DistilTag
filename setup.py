49f1cbc88bef6a95f6f3686460ecd079acbbc337
seantyh <seantyh@gmail.com>
2020-09-25 22:44:17 +0800

    update requirements

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='DistilTag',
-    version='0.2.0',
+    version='0.2.1',
     author="NTUGIL LOPE Lab",
     url="https://github.com/lopentu/DistilTag",
     packages=['DistilTag'],
     setup_requires=["wheel"],
-    install_requires=["gdown", "torch", "transformers", "numpy"],
+    install_requires=["gdown", "torch>=1.6", "transformers>=3.2", "numpy"],
     license='GNU GPLv3',
     long_description=open('README.md').read()
 )
