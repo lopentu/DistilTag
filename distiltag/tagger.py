5469e64f0948eb06d29beb2b6043b711c4eec5b1
seantyh <seantyh@gmail.com>
2021-05-05 23:04:41 +0800

    add soft tag

--- distiltag/tagger.py
+++ distiltag/tagger.py
@@ -5,6 +5,7 @@ import numpy as np
 from .download import get_model_path
 from transformers import DistilBertTokenizerFast
 from transformers import DistilBertForTokenClassification
+from collections import Counter
 
 HALF2FULL = dict((i, i + 0xFEE0) for i in range(0x21, 0x7F))
 HALF2FULL[0x20] = 0x3000
@@ -20,6 +21,7 @@ class DistilTag:
         self.model.to(self.device)
         self.tokenizer = DistilBertTokenizerFast.from_pretrained(model_path)
         self.label_map = self.get_label_map(model_path)
+        self.pos_list = sorted(list(set(x[2:] for x in self.label_map.values())))
 
     def get_label_map(self, model_path):
         label_path = os.path.join(model_path, "labels.txt")
@@ -59,7 +61,7 @@ class DistilTag:
             tagged.append(tuple(buf))
         return tagged
 
-    def tag(self, text):
+    def tag(self, text, return_logits=False):
         text = text.translate(HALF2FULL)
         label_map = self.label_map
         sentences = self.make_sentences(text)
@@ -79,12 +81,15 @@ class DistilTag:
             outs = self.model(
                     input_ids=batch["input_ids"],
                     attention_mask=batch["attention_mask"])[0]
+            logits = outs
             preds = np.argmax(outs.cpu().numpy(), axis=2)
 
         batch_size, seq_len = preds.shape
         input_ids = batch["input_ids"].cpu().numpy()
         preds_list = [[] for _ in range(batch_size)]
         tokens_list = [[] for _ in range(batch_size)]        
+        if return_logits:
+            logits_list = [[] for _ in range(batch_size)]
         offset_mapping = batch["offset_mapping"]
         for i in range(batch_size):
             for j in range(1,(seq_len-1)):                
@@ -92,15 +97,101 @@ class DistilTag:
                     continue
                 omap = offset_mapping[i][j]
                 preds_list[i].append(label_map[preds[i][j]])
+                if return_logits:
+                    logits_list[i].append(logits[i, j, :])
                 token = sentences[i][omap[0]:omap[1]]
                 tokens_list[i].append(token)
         
         tagged_list = [self.make_tagged(tokens, pred)
                         for tokens, pred
                         in zip(tokens_list, preds_list)]
-
-        return tagged_list
+        
+        if return_logits:
+            logits_list = [torch.stack(tensor_xs) for tensor_xs in logits_list]
+            return tagged_list, logits_list, tokens_list
+        else:
+            return tagged_list
 
     def print_tagged_list(self, tagged_list):
         for tagged_x in tagged_list:
             print("\u3000".join(f"{x[0]}({x[1]})" for x in tagged_x))
+    
+    def soft_tag(self, text, Tw=2, Tp=2):
+        out = self.tag(text, return_logits=True)
+        logits = out[1]
+        tokens = out[2]
+        n_seq = len(logits)
+        
+        word_logits = [[] for _ in range(n_seq)]
+        pos_logits = [[] for _ in range(n_seq)]
+        for seq_i in range(n_seq):
+            seq_logits = logits[0]
+            M = seq_logits.shape[0]
+            wb_table = np.zeros(shape=(M, 2), dtype=np.float32)
+            pos_table = np.zeros(shape=(M, len(self.pos_list)), dtype=np.float32)
+
+            label_map = self.label_map
+            # word boundary
+            wb = [idx for idx in label_map.keys() if label_map[idx].startswith("B-")]
+            wi = [idx for idx in label_map.keys() if label_map[idx].startswith("I-")]
+            wb_table[:, 0] = seq_logits[:, wb].mean(axis=1)
+            wb_table[:, 1] = seq_logits[:, wi].mean(axis=1)
+            wb_table = torch.from_numpy(wb_table)
+
+            # pos
+            for pos_i, pos_x in enumerate(self.pos_list):
+                mask_idx = [idx for idx in label_map.keys() if label_map[idx].endswith(pos_x)]
+                pos_table[:, pos_i] = seq_logits[:, mask_idx].mean(axis=1)
+            pos_table = torch.from_numpy(pos_table)
+            
+            word_logits[seq_i] = wb_table
+            pos_logits[seq_i] = pos_table
+
+        return word_logits, pos_logits, tokens
+
+    def logits_to_probs(self, logits, T=1):
+        probs = [torch.softmax(logit_x/T, axis=1)
+                 for logit_x in logits]
+        return probs
+
+    def decode_soft_tag(self, word_logits, pos_logits, tokens, 
+            Tw=2, Tp=2, wcut=0.5):
+        word_probs = self.logits_to_probs(word_logits, Tw)
+        pos_probs = self.logits_to_probs(pos_logits, Tp)
+        
+        seq_words = []
+        for word_prob, pos_prob, tokens_seq in zip(word_probs, pos_probs, tokens):
+            word_list = []
+            buf = []
+            for tok_i in range(len(tokens_seq)):
+                p_wb = word_prob[tok_i, 0]
+                if p_wb > wcut and buf:
+                    word = "".join([tokens_seq[i] for i in buf])
+                    pos_idx = pos_prob[buf, :].mean(axis=0).argmax()
+                    pos = self.pos_list[pos_idx]
+                    word_list.append((word, pos))
+                    buf = []
+                buf.append(tok_i)
+            word = "".join([tokens_seq[i] for i in buf])
+            pos_idx = pos_prob[buf, :].mean(axis=0).argmax()
+            pos = self.pos_list[pos_idx]
+            word_list.append((word, pos))
+
+            seq_words.append(word_list)
+        return seq_words
+    
+    def print_soft_tag(self, word_logits, pos_logits, tokens, Tw=2, Tp=2):
+        word_probs = self.logits_to_probs(word_logits, Tw)
+        pos_probs = self.logits_to_probs(pos_logits, Tp)
+
+        def show_pos(i):
+            return self.pos_list[i].replace("CATEGORY", "")[:3]
+        for word_prob, pos_prob, tokens_seq in zip(word_probs, pos_probs, tokens):
+            for tok_i, tok_x in enumerate(tokens_seq):    
+                probs, inds = pos_prob[tok_i, :].topk(5)
+                wb = word_prob[tok_i, 0]
+                pos_strips = [f"{show_pos(i):>3s}_{prob:.2f}" 
+                                for i, prob in zip(inds, probs)]
+                out_str = tok_x + f"_{wb:.2f} " + "/".join(pos_strips)
+                print(out_str)
+            print("")
